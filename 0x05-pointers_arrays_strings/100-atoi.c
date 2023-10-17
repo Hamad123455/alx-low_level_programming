037c2b25007774e347c6fb6fffca842a515981fe
Hamad123455 <hamad.mo888888@gmail.com>
2023-10-17 10:06:08 -0700

    convert string to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,9 +2,10 @@
 
 /**
  * _atoi - converts a string to an integer
- * @s: string to be converted
  *
- * return: the int converted from the string
+ * @s: string to inpit parameter
+ *
+ * Return: converted integer from the string
  */
 int _atoi(char *s)
 {
@@ -13,7 +14,7 @@ int _atoi(char *s)
 	i = 0;
 	d = 0;
 	n = 0;
-	len =0;
+	len = 0;
 	f = 0;
 	digit = 0;
 
@@ -30,17 +31,16 @@ int _atoi(char *s)
 			digit = s[i] - '0';
 			if (d % 2)
 				digit = -digit;
-			n = n * 10 + digit
+			n = n * 10 + digit;
 				f = 1;
-			if (s[i + 1] < '0' || s[i +1] > '9')
+			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 			f = 0;
 		}
 		i++;
 	}
 
-	if ( f == 0)
-		return(0);
-	
+	if (f == 0)
+		return (0);
 	return (n);
 }
