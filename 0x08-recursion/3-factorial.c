0d01cd9f70179ebf371be6eab3ee8094ab68640e
Hamad123455 <hamad.mo888888@gmail.com>
2023-10-25 10:09:46 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function that return the value of x
+ * raised to the power of y
+ * @x: value to be manipulated
+ * @y: manipulater
+ *
+ * Return: Sum as int
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y > 0)
+		return (-1);
+	if (!y)
+		return (1);
+	return (x * _pow_recursion(x, --y));
+}
